9db5f443035dfe7ec04fa1990ce4184fb4a5062e
Mathieu MORGAT <morgatmathieu@gmail.com>
2023-04-12 11:02:41 +0200

    Created template folder + route, model and template for pizzas

--- webpizza/applipizza/views.py
+++ webpizza/applipizza/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from applipizza.models import Pizza
+
 # Create your views here.
+def pizzas(request) :
+    lesPizzas = Pizza.objects.all()
+    return render(request,
+    "applipizza/pizzas.html",
+    {"pizzas" : lesPizzas }
+    )
