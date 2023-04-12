43fc6fb44c2713c449cb8caeccf196c23ba0ea2e
Mathieu MORGAT <morgatmathieu@gmail.com>
2023-04-12 10:20:38 +0200

    added models in models.py

--- webpizza/applipizza/models.py
+++ webpizza/applipizza/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Ingredient(models.Model) :
+    idIngredient = models.AutoField(primary_key = True)
+
+    nomIngredient = models.CharField(max_length = 50, verbose_name = 'le nom de l\'ingredient ')
+    
+    def __str__(self) -> str:
+        return 'ingredient ' + self.nomIngredient
+
+class Pizza(models.Model) :
+    idPizza = models.AutoField(primary_key = True)
+
+    nomPizza = models.CharField(max_length = 50, verbose_name = 'le nom de cette pizza ')
+
+    prix = models.DecimalField(max_digits = 4, decimal_places = 2, verbose_name = 'le prix' )
+
+    def __str__(self) -> str:
+        return 'pizza' + self.nomPizza + ' (prix = ' + str(self.prix) + '$)'
